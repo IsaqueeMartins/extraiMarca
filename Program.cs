61e459e2d2288ce93694088c6197a5e1c005f5a7
Isaque Martins <isaque.martins@redekrill.com>
2024-07-25 17:07:03 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using ClosedXML.Excel;
+using extraiMarca;
+
+namespace LerPlanilha
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string filePathName = System.IO.Directory.GetCurrentDirectory() + "\\planilhas\\produtos_sem_marca25072024_1534.xlsx";
+            if (File.Exists(filePathName))
+            {
+                List<MarcaModel> lista = new List<MarcaModel>();
+                var workbook = new XLWorkbook(filePathName);
+                var nonEmptyDataRows = workbook.Worksheet(1).RowsUsed();
+
+                foreach (var dataRow in nonEmptyDataRows)
+                {
+                    if(dataRow.RowNumber() > 1)
+                    {
+                        var marca = new MarcaModel();
+                        marca.ProdutoKey = dataRow.Cell(1).Value.ToString();
+                        marca.Descricao = dataRow.Cell(2).Value.ToString();
+
+                        lista.Add(marca);
+                    }
+                }
+                Console.WriteLine(JsonSerializer.Serialize(lista));
+            }
+            }
+        }
+    }
