bb8e48f52a0cdb6c18d940554c288bcb389911ea
Isaque Martins <isaque.martins@redekrill.com>
2024-07-26 15:37:17 -0300

    alterado propriedade de planilha para copiar sempre

--- Program.cs
+++ Program.cs
@@ -3,34 +3,19 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using ClosedXML.Excel;
-using extraiMarca;
+using DocumentFormat.OpenXml.Bibliography;
+using extraiMarca.Classes;
+using extraiMarca.Model;
 
 namespace LerPlanilha
 {
     class Program
     {
-        static void Main(String[] args)
+        public static void Main(string[] args)
         {
-            string filePathName = System.IO.Directory.GetCurrentDirectory() + "\\planilhas\\produtos_sem_marca25072024_1534.xlsx";
-            if (File.Exists(filePathName))
-            {
-                List<MarcaModel> lista = new List<MarcaModel>();
-                var workbook = new XLWorkbook(filePathName);
-                var nonEmptyDataRows = workbook.Worksheet(1).RowsUsed();
-
-                foreach (var dataRow in nonEmptyDataRows)
-                {
-                    if(dataRow.RowNumber() > 1)
-                    {
-                        var marca = new MarcaModel();
-                        marca.ProdutoKey = dataRow.Cell(1).Value.ToString();
-                        marca.Descricao = dataRow.Cell(2).Value.ToString();
-
-                        lista.Add(marca);
-                    }
-                }
-                Console.WriteLine(JsonSerializer.Serialize(lista));
-            }
-            }
+            lerPlanilhas.lerPlanilhasDescricao();
+            lerPlanilhas.lerPlanilhasMarca();
+            comparaPlanilhas.recebeValores();
         }
     }
+}
