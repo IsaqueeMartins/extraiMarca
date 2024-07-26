f36121eca64ec7bacda93bd819b1dc09ed675a0b
Isaque Martins <isaque.martins@redekrill.com>
2024-07-26 15:36:28 -0300

    Adicionado foreach com if para que todos os valores da lista sejam checados mesmo que uma das listas seja maior

---
+++ Classes/comparaPlanilhas.cs
@@ -0,0 +1,31 @@
+﻿namespace extraiMarca.Classes
+{
+    internal class comparaPlanilhas
+    {
+        public static void recebeValores()
+        {
+            var listaDescricao = lerPlanilhas.lerPlanilhasDescricao();
+            var listaMarca = lerPlanilhas.lerPlanilhasMarca();
+
+            foreach (var marca in listaMarca)
+            {
+                bool marcaEncontrada = false;
+
+
+                foreach (var descricao in listaDescricao)
+                {
+                    if (descricao.Descricao.Contains(marca.Marca))
+                    {
+                        Console.WriteLine($" {descricao.Descricao} ||| {marca.Marca}");
+                    }
+                }
+
+                if (!marcaEncontrada)
+                {
+                    Console.WriteLine($"{marca.Marca}");
+                }
+            }
+        }
+    }
+}
+
