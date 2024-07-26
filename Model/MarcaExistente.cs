28f11cae3e5e0075dfc3f763a0eb067b229f8749
Isaque Martins <isaque.martins@redekrill.com>
2024-07-26 11:34:28 -0300

    Model configurado para ambos os arquivos de comparacao

---
+++ Model/MarcaExistente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extraiMarca.Model
+{
+    internal class MarcaExistente
+    {
+        [Description("Marca")]
+        public string Marca { get; set; }
+    }
+}
---
+++ Model/MarcaExtrair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extraiMarca.Model
+{
+    public class MarcaExtrair
+    {
+        [Description("Código")]
+        public string ProdutoKey { get; set; }
+
+        [Description("Descrição")]
+
+        public string Descricao { get; set; }
+    }
+}
